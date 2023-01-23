90230194df2fa6220cfceaa6fe6d2a34f1e85d57
Timothy Axford <t.axford2@yahoo.com>
2023-01-22 20:21:55 -0500

    Added a console writeline calling the member variables as volume = length*height*width

--- Properties.Net5.0/Box.cs
+++ Properties.Net5.0/Box.cs
@@ -14,5 +14,10 @@ namespace Properties.Net5._0
         public int width;
         public int volume;
 
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Lenght is {0} and height is {1} and width is {2} so the volumeis {3}", length, height, width, volume = length*height*width);
+        }
+
     }
 }
