df0f25eac64a78d304a19e34b72b1cb9b2f78f21
Timothy Axford <t.axford2@yahoo.com>
2023-01-22 20:24:55 -0500

    Gave an instance of box a value and ran a DisplayInfo to show the volume.

--- Properties.Net5.0/Program.cs
+++ Properties.Net5.0/Program.cs
@@ -7,8 +7,11 @@ namespace Properties.Net5._0
         static void Main(string[] args)
         {
             Box box = new Box();
+            box.length = 3;
+            box.height = 4;
+            box.width = 5;
 
-            Console.WriteLine("Hello World!");
+            box.DisplayInfo();
         }
     }
 }
