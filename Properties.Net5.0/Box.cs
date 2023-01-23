d710345e5618cab818e382b1e6790c222eb5a205
Timothy Axford <t.axford2@yahoo.com>
2023-01-22 20:16:40 -0500

    Add project files.

---
+++ Properties.Net5.0/Box.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties.Net5._0
+{
+    internal class Box
+    {
+    }
+}
