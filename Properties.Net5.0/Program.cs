c1af0ad6d31efcfa146c93c49d991d2ddf13aff9
Timothy Axford <t.axford2@yahoo.com>
2023-02-03 10:27:50 -0500

    Added Console.Writeline to call out the box.Width

--- Properties.Net5.0/Program.cs
+++ Properties.Net5.0/Program.cs
@@ -10,9 +10,9 @@ namespace Properties.Net5._0
             //box.length = 3;
             box.SetLength(4);
             box.Height = -4;
-            box.width = 5;
-            Console.WriteLine("Box s volume is " + box.GetVolume());
-
+            box.Width = 5;
+            Console.WriteLine("Box s volume is " + box.Width);
+            Console.WriteLine("Box s width is " + box.GetVolume());
             box.DisplayInfo();
         }
     }
