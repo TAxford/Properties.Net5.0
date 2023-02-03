26e90c60638787b301ef8b081e8c424bc22c3d38
Timothy Axford <t.axford2@yahoo.com>
2023-02-03 08:45:38 -0500

    Changed Program cs box.Height to -4 and SetLength to 4 and ran program to test that it did indeed work.

--- Properties.Net5.0/Program.cs
+++ Properties.Net5.0/Program.cs
@@ -8,8 +8,8 @@ namespace Properties.Net5._0
         {
             Box box = new Box();
             //box.length = 3;
-            box.SetLength(-3);
-            box.Height = 4;
+            box.SetLength(4);
+            box.Height = -4;
             box.width = 5;
             Console.WriteLine("Box s volume is " + box.GetVolume());
 
